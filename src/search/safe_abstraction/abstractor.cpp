b3061ff7cba79020ecc66df2c93ab1fa9b09a02e
Joan Moser <Joan@qualit.ch>
2024-09-23 14:13:55 +0200

    Created (pseudo) skeleton code for the creation of the freeDTGs

--- src/search/safe_abstraction/abstractor.cpp
+++ src/search/safe_abstraction/abstractor.cpp
@@ -18,9 +18,57 @@ void abstractor::find_safe_variables(std::shared_ptr<AbstractTask> orignial_task
   //Get the DTGs for the task
   domain_transition_graph::DTGFactory dtg_factory(task_proxy, collect_side_effects, pruning_condition);
   std::vector<std::unique_ptr<DomainTransitionGraph>> dtgs = dtg_factory.build_dtgs();
+
+  std::vector<std::unique_ptr<DomainTransitionGraph>> free_dtgs = get_free_dtgs(dtgs);
 }
 
-domain_transition_graph abstractor::get_free_domain_transition_graph(std::vector<std::unique_ptr<DomainTransitionGraph>> dtgs)
+std::vector<std::unique_ptr<DomainTransitionGraph>> abstractor::get_free_domain_transition_graph(std::vector<std::unique_ptr<DomainTransitionGraph>> dtgs)
 {
+	std::vector<std::unique_ptr<DomainTransitionGraph>> free_dtgs;
 
+	for (const auto &dtg : dtgs)
+	{
+        //TODO: Create freeDTG object
+        for (const auto &node : dtg->nodes)
+		{
+        	bool nodeHandled = false;
+            for (const auto &transition : node.transitions)
+			{
+                bool is_free_transition = true;
+
+                // Check preconditions and effects
+                for (const auto &label : transition.labels)
+				{
+                	for (const auto &precon : label.preconditions)
+                    {
+                        //Skip if variable is not a diffrent variable
+                        if (var_id != precon.local_var) //TODO: Figure out how local vs global vars behave
+                        {
+                        	is_free_transition = false;
+                    		break;
+                        }
+                    }
+                    if (!is_free_transition) //If it is not a free transition we don't have to check for postconditions
+               		{
+                    	for (const auto &postcon : label.postconditions)
+                    	{
+                        	//Skip if variable is not a diffrent variable
+                        	if (var_id != precon.local_var) //TODO: Figure out how local vs global vars behave
+                        	{
+                        		is_free_transition = false;
+                    			break;
+                        	}
+                    	}
+                    }
+                }
+
+                if (is_free_transition) {
+                    //TODO: Add free nodes and free transitions to freeDTG
+                }
+            }
+        }
+        //TODO: Add new freeDTG to free_dtgs
+	}
+
+	return free_dtgs;
 }
