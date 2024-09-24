9a098ae96506314bfa98cd4c122df576e1d3d6ca
Joan Moser <Joan@qualit.ch>
2024-09-24 12:13:40 +0200

    Reformatted the isStronglyConnected function and commented code throughout free_domain_transition_graph.cc for easier legibility

--- src/search/safe_abstraction/free_domain_transition_graph.cc
+++ src/search/safe_abstraction/free_domain_transition_graph.cc
@@ -31,60 +31,41 @@ void freeDTG::externallyCaused(int val)
 
 /*
 Based on https://www.geeksforgeeks.org/connectivity-in-a-directed-graph/
+
+Checks if the targetValues are strongly connected within the freeDTG.
+Note that they may travel via non-target values to reach eachother.
  */
 bool freeDTG::isStronglyConnected(std::list<int> targetValues)
 {
+	//Trivial case. Not sure if true or false should be returned
     if (targetValues.size() == 0) {return true;}
-    std::vector<bool> visited(numVal, false);
 
-    freeDTG::DFS(targetValues.front(), &visited);
+    //Check if all targetValues are reachable from the first target value v
+    if (!freeDTG::isReachable(targetValues.front(), targetValues)){return false;}
 
-    for (int i = 0; i < visited.size(); i++)
-    {
-        if (visited[i] == false)
-        {
-            for (int target : targetValues)
-            {
-                if (target == i)
-                {
-                    //If a target variable wasn't visited we already know its not strongly connected.
-                    std::cout << "freeDTG is not strongly connected: couldn't reach: " << i << " from: " << targetValues.front() << std::endl;
-                    return false;
-                }
-            }
-        }
-    }
-
+	//Invert the freeDTG a->b => a<-b
     freeDTG inversion = freeDTG::getTranspose();
 
-    std::vector<bool> inversionVisited(targetValues.size(), false);
+    /*
+    Check if all targetValues are reachable from the first target value v in the inversion.
+    If this is the case, we know that in the original (uninverted) free DTG, all targetValues can reach v.
 
-    inversion.DFS(targetValues.front(), &inversionVisited);
-
-    for (int i = 0; i < inversionVisited.size(); i++)
-    {
-        if (inversionVisited[i] == false)
-        {
-            for (int target : targetValues)
-            {
-                if (target == i)
-                {
-                    std::cout << "freeDTG is not strongly connected: (inverted) couldn't reach: " << i << " from: " << targetValues.front() << std::endl;
-                    return false;
-                }
-            }
-        }
-    }
+    This together with the knowledge that v can reach all target values means that all target values are
+    reachable by any target value via v.
+     */
+    if (!inversion.isReachable(targetValues.front(), targetValues)){return false;}
 
     return true;
 }
 
+//Simple recursive DFS
 void freeDTG::DFS(int v, std::vector<bool> *visited)
 {
     (*visited)[v] = true;
 
     for (int value : transitions[v])
     {
+   		//Don't deepen if value was already visited (avoids getting stuck in cycles).
         if (!(*visited)[value])
         {
             DFS(value, visited);
@@ -92,6 +73,7 @@ void freeDTG::DFS(int v, std::vector<bool> *visited)
     }
 }
 
+//Inverts the DTG such that transision a->b becomes a<-b.
 freeDTG freeDTG::getTranspose()
 {
     freeDTG inversion(variable, numVal);
@@ -105,20 +87,21 @@ freeDTG freeDTG::getTranspose()
     return inversion;
 }
 
-bool freeDTG::isReachable(int value, std::list<int> targetValues)
+//Check if targetValues are reachable by startingValue in the DTG
+bool freeDTG::isReachable(int startingValue, std::list<int> targetValues)
 {
     if (targetValues.size() == 0) {return true;}
     std::vector<bool> visited(numVal, false);
 
-    freeDTG::DFS(value, &visited);
+    freeDTG::DFS(startingValue, &visited); //Perform DFS
 
     for (int i = 0; i < visited.size(); i++)
     {
-        if (visited[i] == false)
+        if (visited[i] == false) //If a value wasn't visited its not reachable from the starting value
         {
             for (int target : targetValues)
             {
-                if (target == i)
+                if (target == i) //If a target value isn't reachable we retun false.
                 {
                     //std::cout << "couldn't reach: " << i << " from: " << targetValues.front() << std::endl;
                     return false;
@@ -126,7 +109,6 @@ bool freeDTG::isReachable(int value, std::list<int> targetValues)
             }
         }
     }
-
     return true;
 }
 
